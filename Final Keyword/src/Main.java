25e2b0879f88c7dff5db804222aced56f4943dcd
Jenul Ferdinand <jenul15ferdinand@gmail.com>
2022-10-23 21:44:26 +1100

    Final Keyword

---
+++ Final Keyword/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		// Initialise final local variable of PI
+		final double PI = 3.14159265359;
+
+		// PI = 1; // The 'final' local variable cannot be changed.
+
+		// Output the value of PI
+		System.out.println(PI);
+	}
+}
